c5889014f048b24fc4ab3964c9b07586c5f6207a
rohith-sheregar <rohithraghu3228@gmail.com>
2026-08-15 07:53:14 +0530

    Fix: remove UTF-8 BOM from HealthController.java (caused javac compile error on Render)

--- src/main/java/com/feedback/feedback/system/controller/HealthController.java
+++ src/main/java/com/feedback/feedback/system/controller/HealthController.java
@@ -1,4 +1,4 @@
-﻿package com.feedback.feedback.system.controller;
+package com.feedback.feedback.system.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
